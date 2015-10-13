67b5054d10f0e15b55c6827a1f4bf5761c4740d0
Chirag Khandelwal <chirag.khandelwal2911@gmail.com>
2015-10-13 13:18:48 +0530

    Its first Commit

---
+++ app/src/main/java/geofence/killerrech/com/GeoAlert/ManualSearchFragment.java
@@ -0,0 +1,124 @@
+package geofence.killerrech.com.GeoAlert;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+public class ManualSearchFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    GoogleMap googleMap;
+    View view;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    // TODO: Rename and change types and number of parameters
+    public static ManualSearchFragment newInstance(String param1, String param2) {
+        ManualSearchFragment fragment = new ManualSearchFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ManualSearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+
+        view = inflater.inflate(R.layout.fragment_manual_search, container,
+                false);
+
+
+
+        googleMap =    ((SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+//                googleMap = ((SupportMapFragment) getChildFragmentManager()
+//                        .findFragmentById(R.id.map)).getMap();
+
+
+
+
+        // Loading map
+
+
+        // Changing map type
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        // googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        // googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        // googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+
+        // Showing / hiding your current location
+        googleMap.setMyLocationEnabled(true);
+
+        // Enable / Disable zooming controls
+        googleMap.getUiSettings().setZoomControlsEnabled(false);
+
+        // Enable / Disable my location button
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        // Enable / Disable Compass icon
+        googleMap.getUiSettings().setCompassEnabled(true);
+
+        // Enable / Disable Rotate gesture
+        googleMap.getUiSettings().setRotateGesturesEnabled(true);
+
+        // Enable / Disable zooming functionality
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        // lets place some 10 random markers
+        System.out.println("enter in map");
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+
+
+}
